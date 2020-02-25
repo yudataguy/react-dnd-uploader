f452086900dba6a4a3a2c710fa040bfaa4fe717c
Othmane Tayab <othmane.tayab@unitedremote.com>
2020-02-25 14:24:34 +0100

    [feat/issue-13] Added the uploadPath prop to enable default uploading and an autoUpload prop to upload the files on the drop event

--- src/App.js
+++ src/App.js
@@ -3,49 +3,19 @@ import DnD from "./components/DnD";
 import "./App.css";
 
 import { AppContext } from "./context/AppProvider";
-import ApiService from "./ApiService";
-import { createPreviews } from "./utils";
-
-const apiService = new ApiService();
 
 export default function App() {
   const context = React.useContext(AppContext);
-  const fileInput = React.useRef();
-  const [files, setFiles] = React.useState(undefined);
-  const [uploading, setUploading] = React.useState(false);
-  const [autoUpload, setAutoUpload] = React.useState(false); // default value should comes from props
 
-  const handleChange = () => {
-    createPreviews(fileInput.current.files, context);
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (context.files) {
-      Object.values(context.files).forEach(file => {
-        apiService.uploadFile(file, setUploading, setFiles, context.setContext);
-      });
-    }
-  };
-
-  React.useEffect(() => {
-    if (autoUpload) {
-      if (files) {
-        Object.values(files).forEach(file => {
-          apiService.uploadFile(
-            file,
-            setUploading,
-            setFiles,
-            context.setContext
-          );
-        });
-      }
-    }
-  }, [files]);
-
+  const { uploading } = context;
   return (
     <div className="App">
-      <DnD className="drag-and-drop" preview handleSubmit={handleSubmit}>
+      <DnD
+        className="drag-and-drop"
+        preview
+        uploadUrl="http://localhost:3001/api/photo"
+        autoUpload
+      >
         <div>
           <div>
             <div style={{ marginBottom: "1rem" }}>
@@ -53,15 +23,6 @@ export default function App() {
                 ? 'Click "Choose Files" or Drag and Drop files here to upload'
                 : "Uploading..."}
             </div>
-            {!uploading && (
-              <input
-                type="file"
-                name="files"
-                multiple
-                ref={fileInput}
-                onChange={handleChange}
-              />
-            )}
           </div>
         </div>
       </DnD>
